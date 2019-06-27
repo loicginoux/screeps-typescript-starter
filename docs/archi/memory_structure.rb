8d2365f5bd981afa4da84a23426a08cb90eb280e
loic <lginoux@gmail.com>
2019-06-27 14:37:57 +0200

    memory data example

---
+++ docs/archi/memory_structure.rb
@@ -0,0 +1,34 @@
+{
+	rooms: [
+		roomName: {
+      avoid: false,
+      miningSites: [
+        {
+          harvesters: [:id],
+          truck: [:id],
+          container: :id,
+        }
+      ],
+      extractorSites: [
+        {
+          harvesters: [:id],
+          truck: [:id],
+          container: :id,
+          end_cool_down_at: :time,
+          pos: [1,2]
+        }
+      ]
+    }
+  ],
+  currentDirective: :upgrade
+	creeps: [
+		creepName: {
+
+    }
+  ]
+}
+https://screeps.com/a/#!/room/shard3/W23S25
+
+https://screeps.com/a/#!/room/shard3/W31N24
+
+https://screeps.com/a/#!/room/shard3/W29S26
