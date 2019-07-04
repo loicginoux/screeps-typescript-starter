4627c2166a601818183ec6c46c1fbcfe5c8e01fc
loic <lginoux@gmail.com>
2019-07-04 17:36:35 +0200

    upgrader should take energy from containers

--- src/roles/RoleUpgrader.ts
+++ src/roles/RoleUpgrader.ts
@@ -4,13 +4,29 @@ import { Utils } from 'utils/Utils';
 export class RoleUpgrader {
   public static newTask(creep: Creep): void {
 
-    let energySources = _.sortBy(creep.room.find(FIND_SOURCES_ACTIVE), s => creep.pos.getRangeTo(s))
-
-    if (energySources.length > 0 && creep.carry.energy < creep.carryCapacity) {
-      if (creep.pos.getRangeTo(energySources[0]) > 1) {
-        creep.task = Tasks.goTo(energySources[0].pos)
+    if (creep.carry.energy < creep.carryCapacity) {
+      let containers = creep.room.find(FIND_STRUCTURES, {
+        filter: (i) => i.structureType == STRUCTURE_CONTAINER && i.store[RESOURCE_ENERGY] > 0
+      }) as StructureContainer[];
+      containers = _.sortBy(containers, s => creep.pos.getRangeTo(s))
+      // get from containers first
+      if (!!containers) {
+        if (creep.pos.getRangeTo(containers[0]) > 1) {
+          creep.task = Tasks.goTo(containers[0])
+        } else {
+          creep.task = Tasks.withdraw(containers[0])
+        }
       } else {
-        creep.task = Tasks.harvest(energySources[0])
+        // else get from source directly
+        let sources = creep.room.find(FIND_SOURCES_ACTIVE) as Source[]
+        sources = _.sortBy(sources, s => creep.pos.getRangeTo(s))
+        if (!!sources) {
+          if (creep.pos.getRangeTo(sources[0]) > 1) {
+            creep.task = Tasks.goTo(sources[0])
+          } else {
+            creep.task = Tasks.harvest(sources[0])
+          }
+        }
       }
     } else {
       const controller = creep.room.controller
